1d48ce441fd78c2f2d864ed9f402ed1240bdf549
Muntaha <muntaha1629@gmail.com>
2026-02-14 14:09:56 +0500

    more file work updated

--- script.js
+++
@@ -1,6 +0,0 @@
-const nav = document.querySelector('nav');
-const toggleBtn = document.querySelector('.fa-bars'); // Add a <i class="fa fa-bars"></i> in header
-toggleBtn.addEventListener('click', () => {
-  nav.classList.toggle('active');
-});
-
